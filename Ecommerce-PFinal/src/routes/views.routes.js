36607b8e09f0f41745573b11145927eca9857d10
Rafael Medina <rafaelmedina350@hotmail.com>
2023-09-17 23:37:40 -0300

    fix

--- Ecommerce-PFinal/src/routes/views.routes.js
+++ Ecommerce-PFinal/src/routes/views.routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/",(req,res)=>{
-    res.render("home");
+    res.render("index");
 });
 
 router.get("/registro",(req,res)=>{
